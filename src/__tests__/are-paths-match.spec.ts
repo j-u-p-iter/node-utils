b98cda14a3b83883c72a0c7f2fb27ed4e4be8e18
kirill <pn79122821224@gmail.com>
2018-12-22 07:42:10 +0100

    fix(package): fixes linting issues

--- src/__tests__/are-paths-match.spec.ts
+++ src/__tests__/are-paths-match.spec.ts
@@ -1,18 +1,22 @@
-import cases from 'jest-in-case';
+import cases from "jest-in-case";
 
-import { arePathsMatch } from '../.';
+import { arePathsMatch } from "../.";
 
-describe('arePathsMatch(pathPattern, pathToCheck)', () => {
-  cases('detects that basePath matches to pathToCheck', ({ pathPattern }: { [key: string]: string }) => {
-    const pathToCheck = '/some/path/to/compare/with';
+describe("arePathsMatch(pathPattern, pathToCheck)", () => {
+  cases(
+    "detects that basePath matches to pathToCheck",
+    ({ pathPattern }: { [key: string]: string }) => {
+      const pathToCheck = "/some/path/to/compare/with";
 
-    const result = arePathsMatch(pathPattern, pathToCheck);
-    const expected = true;
+      const result = arePathsMatch(pathPattern, pathToCheck);
+      const expected = true;
 
-    expect(result).toBe(expected);
-  }, [
-    { pathPattern: '/some/path/to/compare/with' },
-    { pathPattern: '/some/path/to/compare/:with' },
-    { pathPattern: '/some/path/:to/:compare/w:ith' },
-  ]);
+      expect(result).toBe(expected);
+    },
+    [
+      { pathPattern: "/some/path/to/compare/with" },
+      { pathPattern: "/some/path/to/compare/:with" },
+      { pathPattern: "/some/path/:to/:compare/w:ith" }
+    ]
+  );
 });
--- src/are-paths-match.ts
+++ src/are-paths-match.ts
@@ -1,6 +1,9 @@
-import pathToRegexp from 'path-to-regexp';
+import pathToRegexp from "path-to-regexp";
 
-export const arePathsMatch = (pathPattern: string, pathToCheck: string): boolean => {
+export const arePathsMatch = (
+  pathPattern: string,
+  pathToCheck: string
+): boolean => {
   const pathPatternRegexp = pathToRegexp(pathPattern);
 
   return Boolean(pathToCheck.match(pathPatternRegexp));
